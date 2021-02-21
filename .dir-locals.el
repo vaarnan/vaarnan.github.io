cee640bb14c639c3ff5a66b4beeb5207f32ec081
Vaarnan Drolia <vaarnan@gmail.com>
2021-02-21 11:43:18 +0000

    Add social media profiles to contact-right

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((nil . ((org-html-preamble-format . (("en" "
+<table id='contact-header'>
+  <tr>
+    <td id='contact-right'>
+      <a href='https://twitter.com/vaarnan'>twitter</a>
+      <a href='https://github.com/vellvisher'>github</a>
+      <a href='https://www.linkedin.com/in/vaarnan'>linkedin</a>
+    </td>
+  </tr>
+</table>"))))))
