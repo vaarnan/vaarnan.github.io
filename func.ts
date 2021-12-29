f7188bbdd11343223a0ae00b0e80e376c6f51e82
Vaarnan Drolia <vaarnan@gmail.com>
2021-12-29 06:59:11 +0000

    Human-readable dates
    + Switch to typescript for the project
    + Use recommended typescript configuration for ecma6

---
+++ func.ts
@@ -0,0 +1,28 @@
+const toggleTableOfContentsHidden = function() {
+    var textTOC = document.getElementById("text-table-of-contents")
+    if (textTOC != null && window.getComputedStyle(textTOC).display != null) {
+        textTOC.style.display = window.getComputedStyle(textTOC).display =="none" ? "inline" : "none"
+    }
+}
+
+const linkifyTableOfContents = function() {
+    var heading = document.querySelector("#table-of-contents h2")
+    if (heading != null) {
+        heading.innerHTML = '<a href="javascript:toggleTableOfContentsHidden()">Table of Contents</a>'
+    }
+}
+
+const orgDateToHumanDate = function() {
+    let timestamps = document.querySelectorAll(".timestamp")
+    let options: any = { weekday: "short", year: 'numeric', month: 'short', day: 'numeric' }
+
+    for (let timestamp of timestamps) {
+        console.log(timestamp)
+        timestamp.innerHTML = new Date(timestamp.innerHTML.substring(1,11)).toLocaleDateString(undefined, options)
+    }
+}
+
+const globalOnLoad = function() {
+    linkifyTableOfContents()
+    orgDateToHumanDate()
+}
