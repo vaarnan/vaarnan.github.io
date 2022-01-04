46a8fe576d93a4b78f94b098a4a7e9bb84d23203
Vaarnan Drolia <vaarnan@gmail.com>
2022-01-04 01:48:15 +0000

    Add ability to filter by tag

--- func.js
+++ func.js
@@ -18,7 +18,61 @@ const orgDateToHumanDate = function () {
         timestamp.innerHTML = new Date(timestamp.innerHTML.substring(1, 11)).toLocaleDateString(undefined, options);
     }
 };
+const linkifyTags = function () {
+    let tags = document.querySelectorAll(".tag");
+    for (let tagHeading of tags) {
+        for (let tag of tagHeading.children) {
+            let tagClassName = tag.className;
+            tag.onclick = function (e) { filterByTag(tagClassName); };
+        }
+    }
+};
+// Filter by the provided tag, pass empty string to show all.
+const filterByTag = function (filterTag) {
+    var _a, _b;
+    let articles = document.querySelectorAll(".outline-2");
+    for (let article of articles) {
+        if (filterTag == "") {
+            article.hidden = false;
+            continue;
+        }
+        let tags = (_a = article.querySelector(".tag")) === null || _a === void 0 ? void 0 : _a.children;
+        if (tags == undefined) {
+            article.hidden = true;
+            continue;
+        }
+        var shouldKeep = false;
+        for (let tag of tags) {
+            if (tag.className == filterTag) {
+                shouldKeep = true;
+                break;
+            }
+        }
+        article.hidden = !shouldKeep;
+    }
+    let textTOC = document.getElementById("table-of-contents");
+    if (!textTOC) {
+        console.log("Could not find table of contents element!");
+        return;
+    }
+    if (filterTag == "") {
+        textTOC.hidden = false;
+        return;
+    }
+    textTOC.hidden = true;
+    var tagFilter = document.createElement("span");
+    tagFilter.className = "tag";
+    tagFilter.innerHTML = "<span>" + filterTag + " X" + "</span>";
+    tagFilter.onclick = function (e) {
+        // TODO: Bug when clearing multiple tags, should instead filter on
+        // the remaining tags. Would need marking tags specially.
+        filterByTag("");
+        tagFilter.remove();
+    };
+    (_b = textTOC.parentNode) === null || _b === void 0 ? void 0 : _b.insertBefore(tagFilter, textTOC);
+};
 const globalOnLoad = function () {
     linkifyTableOfContents();
     orgDateToHumanDate();
+    linkifyTags();
 };
--- func.ts
+++ func.ts
@@ -21,7 +21,64 @@ const orgDateToHumanDate = function() {
     }
 }
 
+const linkifyTags = function() {
+    let tags = document.querySelectorAll(".tag")
+    for (let tagHeading of tags) {
+        for (let tag of tagHeading.children) {
+            let tagClassName = tag.className;
+            (<HTMLElement>tag).onclick = function(e) {filterByTag(tagClassName) }
+        }
+    }
+}
+
+// Filter by the provided tag, pass empty string to show all.
+const filterByTag = function(filterTag: String) {
+    let articles = document.querySelectorAll(".outline-2")
+    for (let article of articles) {
+        if (filterTag == "") {
+            (<HTMLElement>article).hidden = false
+            continue
+        }
+        let tags = article.querySelector(".tag")?.children
+        if (tags == undefined) {
+            (<HTMLElement>article).hidden = true
+            continue
+        }
+        var shouldKeep = false
+        for (let tag of tags) {
+            if (tag.className == filterTag) {
+                shouldKeep = true
+                break
+            }
+        }
+        (<HTMLElement>article).hidden = !shouldKeep
+    }
+    let textTOC = document.getElementById("table-of-contents")
+    if (!textTOC) {
+        console.log("Could not find table of contents element!")
+        return
+    }
+    if (filterTag == "") {
+        textTOC.hidden = false
+        return
+    }
+    textTOC.hidden = true
+    var tagFilter = document.createElement("span");
+    tagFilter.className = "tag"
+    tagFilter.innerHTML = "<span>" + filterTag + " X" + "</span>"
+    tagFilter.onclick = function(e) {
+        // TODO: Bug when clearing multiple tags, should instead filter on
+        // the remaining tags. Would need marking tags specially.
+        // Probably should switch filterTag to array too.
+        filterByTag("");
+
+        tagFilter.remove()
+    }
+    textTOC.parentNode?.insertBefore(tagFilter, textTOC)
+}
+
 const globalOnLoad = function() {
     linkifyTableOfContents()
     orgDateToHumanDate()
+    linkifyTags()
 }
